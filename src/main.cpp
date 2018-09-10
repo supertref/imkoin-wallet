4b250bb0de077ff7bdb86a018f32e7f4c2c099cb
Helder Garcia <helder.garcia@gmail.com>
2018-09-10 18:28:19 -0300

    Wallet don't close anymore when remote node is not responding.

--- src/main.cpp
+++ src/main.cpp
@@ -129,10 +129,15 @@ int main(int argc, char* argv[]) {
   app.processEvents();
   qRegisterMetaType<CryptoNote::TransactionId>("CryptoNote::TransactionId");
   qRegisterMetaType<quintptr>("quintptr");
-  if (!NodeAdapter::instance().init()) {
-    return 0;
-  }
   splash->finish(&MainWindow::instance());
+  if (!NodeAdapter::instance().init()) {
+    QString connection = Settings::instance().getConnection();
+    if(connection.compare("remote") == 0) {
+      QMessageBox::warning(nullptr, QObject::tr("Fail"), QObject::tr("Remote node %1 is not responding. Please select another one on Settings->Connection page, or select AUTO to use a local blockchain.").arg(Settings::instance().getCurrentRemoteNode()));
+    } else {
+      return 0;
+    }
+  }
   Updater d;
     d.checkForUpdate();
   MainWindow::instance().show();
